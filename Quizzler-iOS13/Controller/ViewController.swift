54edd33e956b0f600e2b382ee284a988e1b05dcc
Nate Clay <claynath@gmail.com>
2024-07-17 06:35:03 -0400

    Final Quizzler Poject

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -10,62 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var questionView: UILabel!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var Choice1: UIButton!
+    @IBOutlet weak var Choice2: UIButton!
+    @IBOutlet weak var Choice3: UIButton!
     
     var quizBrain = QuizBrain()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         updateUI()
     }
-
-    @IBAction func answerButtonPressed(_ sender: UIButton) {
-        let userAnswer = sender.currentTitle! //True, False
-        quizBrain.checkAnswer(userAnswer)
-        if userAnswer == actualAnswer {
-            questionView.text = "Right!"
-            sender.backgroundColor = UIColor.green
+    
+        @IBAction func answerButtonPressed(_ sender: UIButton) {
+            if quizBrain.getQuestionText() != "Quiz Done!" {
+                let userAnswer = sender.currentTitle! //True, False
+                let userGotItRight = quizBrain.checkAnswer(userAnswer)
+                if userGotItRight {
+                    questionView.text = "Right!"
+                    sender.backgroundColor = UIColor.green
+                    Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+                    quizBrain.nextQuestion()
+                }
+                else {
+                    questionView.text = "Wrong!"
+                    sender.backgroundColor = UIColor.red
+                    Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+                    quizBrain.nextQuestion()
+                }
+                //        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                //            if self.questionNumber < (self.quiz.count - 1) {
+                //                self.questionNumber += 1
+                //                self.updateUI()
+                //            }
+                //            else {
+                //                self.questionView.text = "Quiz Done!"
+                //                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                //                    self.questionNumber = 0
+                //                    self.updateUI()
+                //                }
+                //            }
+                //        }
+                
+            } else {
+                let userAnswer = sender.currentTitle! //True, False
+                let userChoice = quizBrain.gameReset(userAnswer)
+                if userChoice {
+                    questionView.text = "Let's Go!"
+                    sender.backgroundColor = UIColor.green
+                    Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+                    quizBrain.nextQuestion()
+                }
+                else {
+                    questionView.text = "Ok, see You Next Time! Press Start Over if you would like to restart."
+                    sender.backgroundColor = UIColor.green
+                }
+            }
         }
-        else {
-            questionView.text = "Wrong!"
-            sender.backgroundColor = UIColor.red
-        }
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-//            if self.questionNumber < (self.quiz.count - 1) {
-//                self.questionNumber += 1
-//                self.updateUI()
-//            }
-//            else {
-//                self.questionView.text = "Quiz Done!"
-//                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-//                    self.questionNumber = 0
-//                    self.updateUI()
-//                }
-//            }
-//        }
-        
-        if self.questionNumber < (self.quiz.count - 1) {
-            self.questionNumber += 1
-            Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
-        }
-        else {
-            self.questionView.text = "Quiz Done!"
-            self.questionNumber = 0
-            Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
-                        
-        }
-        
-    }
     
     @objc func updateUI() {
-        progressBar.progress = Float(questionNumber+1)/Float(self.quiz.count)
-        questionView.text = quiz[questionNumber].text
-        trueButton.backgroundColor = UIColor.clear
-        falseButton.backgroundColor = UIColor.clear
+            progressBar.progress = quizBrain.getProgress()
+            questionView.text = quizBrain.getQuestionText()
+            Choice1.setTitle(quizBrain.getChoicesText()[0], for: .normal)
+            Choice2.setTitle(quizBrain.getChoicesText()[1], for: .normal)
+            Choice3.setTitle(quizBrain.getChoicesText()[2], for: .normal)
+            scoreLabel.text = "Score: \(quizBrain.getScore())"
+            Choice1.backgroundColor = UIColor.clear
+            Choice2.backgroundColor = UIColor.clear
+            Choice3.backgroundColor = UIColor.clear
     }
     
 }
--- Quizzler-iOS13/Question.swift
+++ Quizzler-iOS13/Question.swift
@@ -10,12 +10,10 @@ import Foundation
 
 struct Question {
     let text: String
-    let choices: [String]
     let answer: String
     
-    init(q:String, a:[String], correctAnswer:String){
+    init(q:String, a:String){
         text = q
-        choices = a
-        answer = correctAnswer
+        answer = a
     }
 }
